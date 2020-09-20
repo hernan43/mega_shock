e7d3faa88671ba65e849ae3735a2fce7dceb78a0
Ray Hernandez <hernan43@gmail.com>
2020-09-20 14:22:08 -0400

    Override devise method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
 
   validates :email, presence: true
 
+  def account_active?
+      return not is_disabled
+  end
+
+  def active_for_authentication?
+    super && account_active?
+  end
+
   def fullname
     return first_name if not first_name.blank? and last_name.blank?
     return last_name if not last_name.blank? and first_name.blank?
